b98d6c341c104a70336a8a27727154d423e91a71
ValeriaGallegoR <valeriagallego20@gmail.com>
2025-03-26 14:40:35 -0400

    calculateCanvasSize.test.ts: Adding test for Error messsage when length or width are negative.

--- src/calculateCanvasSize/calculateCanvasSize.test.ts
+++ src/calculateCanvasSize/calculateCanvasSize.test.ts
@@ -7,9 +7,15 @@ test("should returns correct result", () => {
 
     expect(result).toEqual(1000);
   });
+
+ test("should throw an error message when the length is negative.", () => {
+    expect(() => calculateCanvasSize(-10, 100)).toThrow("Length should be positive");
+  });
+
+  test("should throw an error message when the width is negative.", () => {
+    expect(() => calculateCanvasSize(10, -100)).toThrow("Width should be positive");
+  });
   
-test.todo("should throw an error message when the length is negative.");
-test.todo("should throw an error message when the width is negative.");
 test.todo("should returns correct result when length is zero");
 test.todo("should returns correct result when width is zero");
 });
