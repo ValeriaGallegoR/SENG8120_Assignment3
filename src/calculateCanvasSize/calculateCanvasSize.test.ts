2567a90f62a9dc83f736b832f9b949245f08d459
ValeriaGallegoR <valeriagallego20@gmail.com>
2025-03-26 14:37:56 -0400

    calculateCanvasSize.test.ts: Adding test for Return correct value with positive values.

--- src/calculateCanvasSize/calculateCanvasSize.test.ts
+++ src/calculateCanvasSize/calculateCanvasSize.test.ts
@@ -1,7 +1,13 @@
 import calculateCanvasSize from "./calculateCanvasSize";
 
 describe("calculateCanvasSize", () => {
-test.todo("should returns correct result.");
+
+test("should returns correct result", () => {
+    const result = calculateCanvasSize(10, 100);
+
+    expect(result).toEqual(1000);
+  });
+  
 test.todo("should throw an error message when the length is negative.");
 test.todo("should throw an error message when the width is negative.");
 test.todo("should returns correct result when length is zero");
