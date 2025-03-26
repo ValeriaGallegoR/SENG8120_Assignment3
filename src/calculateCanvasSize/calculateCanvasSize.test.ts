6f83068a856ea24c7d2e80e33d305a43d6ec14b3
ValeriaGallegoR <valeriagallego20@gmail.com>
2025-03-26 14:43:29 -0400

    calculateCanvasSize.test.ts: Adding test for Correct result when length or width are zero.

--- src/calculateCanvasSize/calculateCanvasSize.test.ts
+++ src/calculateCanvasSize/calculateCanvasSize.test.ts
@@ -1,23 +1,29 @@
 import calculateCanvasSize from "./calculateCanvasSize";
 
 describe("calculateCanvasSize", () => {
-
-test("should returns correct result", () => {
+  test("should returns correct result", () => {
     const result = calculateCanvasSize(10, 100);
 
     expect(result).toEqual(1000);
   });
 
- test("should throw an error message when the length is negative.", () => {
+  test("should throw an error message when the length is negative.", () => {
     expect(() => calculateCanvasSize(-10, 100)).toThrow("Length should be positive");
   });
 
   test("should throw an error message when the width is negative.", () => {
     expect(() => calculateCanvasSize(10, -100)).toThrow("Width should be positive");
   });
-  
-test.todo("should returns correct result when length is zero");
-test.todo("should returns correct result when width is zero");
+
+  test("should returns correct result when length is zero", () => {
+    const result = calculateCanvasSize(0, 100);
+
+    expect(result).toEqual(0);
+  });
+
+  test("should returns correct result when width is zero", () => {
+    const result = calculateCanvasSize(10, 0);
+
+    expect(result).toEqual(0);
+  });
 });
-
-
