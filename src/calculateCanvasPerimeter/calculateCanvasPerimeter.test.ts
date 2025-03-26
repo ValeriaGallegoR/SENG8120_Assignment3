69cb4303bfee73d32c505797c1c636c7cc3b3c92
ValeriaGallegoR <valeriagallego20@gmail.com>
2025-03-26 15:09:44 -0400

    calculateCanvasPerimeter.test.ts: Adding tests for negative values.

--- src/calculateCanvasPerimeter/calculateCanvasPerimeter.test.ts
+++ src/calculateCanvasPerimeter/calculateCanvasPerimeter.test.ts
@@ -10,7 +10,12 @@ describe("calculateCanvasPerimeter", () => {
       const result = calculateCanvasPerimeter(0, 0);
       expect(result).toEqual(0);
     });
-    
-  test.todo("should throw an error message when the width is negative and greater than length");
-  test.todo("should throw an error message when the length is negative and greater than width");
+
+  test("should throw an error message when the width is negative and greater than length.", () => {
+      expect(() => calculateCanvasPerimeter(10, -50)).toThrow("Width should be positive or less than length.");
+    });
+
+  test("should throw an error message when the length is negative and greater than width.", () => {
+    expect(() => calculateCanvasPerimeter(-50, 10)).toThrow("Length should be positive or less than width.");
+  });
 });
