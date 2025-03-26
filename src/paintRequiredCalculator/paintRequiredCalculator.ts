80af3be4ef67e0a6b68405a0164e430a5a07991d
ValeriaGallegoR <valeriagallego20@gmail.com>
2025-03-26 15:22:03 -0400

    paintRequiredCalculator.ts: Adding conditionals for negative values.

--- src/paintRequiredCalculator/paintRequiredCalculator.ts
+++ src/paintRequiredCalculator/paintRequiredCalculator.ts
@@ -1,3 +1,12 @@
-export default function paintRequiredCalculator(area: number, coveragePerLiter: number): number {
+export default function paintRequiredCalculator(
+  area: number,
+  coveragePerLiter: number
+) {
+    if (area < 0){
+      throw new Error("Area should be positive");
+    }
+    if (coveragePerLiter <= 0){
+      throw new Error("CoveragePerLiter should be greater than zero");
+    }
   return area / coveragePerLiter;
 }
