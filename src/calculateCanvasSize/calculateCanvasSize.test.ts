78b1de9cad12429351a88cdb2c824d5b3ccf186e
ValeriaGallegoR <valeriagallego20@gmail.com>
2025-03-26 14:34:26 -0400

    calculateCanvasSize.test.ts: Unit tests are all added for: - Return correct value with positive values - Error when length is negative - Error when width is negative - Return correct value when length is zero - Return correct value when width is zero

---
+++ src/calculateCanvasSize/calculateCanvasSize.test.ts
@@ -0,0 +1,11 @@
+import calculateCanvasSize from "./calculateCanvasSize";
+
+describe("calculateCanvasSize", () => {
+test.todo("should returns correct result.");
+test.todo("should throw an error message when the length is negative.");
+test.todo("should throw an error message when the width is negative.");
+test.todo("should returns correct result when length is zero");
+test.todo("should returns correct result when width is zero");
+});
+
+
