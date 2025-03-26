1ea841d50d5a42f41ca1d72bc403934bfc802a3d
ValeriaGallegoR <valeriagallego20@gmail.com>
2025-03-26 15:13:19 -0400

    calculateCanvasPerimeter.ts: Adding conditionals for negative values.

--- src/calculateCanvasPerimeter/calculateCanvasPerimeter.ts
+++ src/calculateCanvasPerimeter/calculateCanvasPerimeter.ts
@@ -2,5 +2,8 @@ export default function calculateCanvasPerimeter(
   length: number,
   width: number
 ) {
+  if (length < 0 || width < 0) {
+    throw new Error('Invalid input: length and width must be non-negative.');
+  }
   return 2 * (length + width);
 }
