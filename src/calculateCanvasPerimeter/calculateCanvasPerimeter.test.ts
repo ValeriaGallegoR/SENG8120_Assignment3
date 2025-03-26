57038ff35b3724938217b1a68ba0be05f159211d
ValeriaGallegoR <valeriagallego20@gmail.com>
2025-03-26 15:06:09 -0400

    calculateCanvasPerimeter.test.ts: Adding test for Correct result when length and width are zero.

--- src/calculateCanvasPerimeter/calculateCanvasPerimeter.test.ts
+++ src/calculateCanvasPerimeter/calculateCanvasPerimeter.test.ts
@@ -6,7 +6,11 @@ describe("calculateCanvasPerimeter", () => {
     expect(result).toEqual(220);
   })
 
-  test.todo("should returns correct result when length and width are zero.");
+  test("should returns correct result when length and width are zero.", () => {
+      const result = calculateCanvasPerimeter(0, 0);
+      expect(result).toEqual(0);
+    });
+    
   test.todo("should throw an error message when the width is negative and greater than length");
   test.todo("should throw an error message when the length is negative and greater than width");
 });
