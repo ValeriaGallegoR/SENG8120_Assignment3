919b1d0bbe1174ebd7da5566e15b7a9c99aa3fba
ValeriaGallegoR <valeriagallego20@gmail.com>
2025-03-26 15:20:55 -0400

    paintRequiredCalculator.test.ts: Adding test with validations.

--- src/paintRequiredCalculator/paintRequiredCalculator.test.ts
+++ src/paintRequiredCalculator/paintRequiredCalculator.test.ts
@@ -7,8 +7,21 @@ describe("paint required calculator", () => {
     expect(result).toEqual(5);
   });
 
-test.todo("should throw an error message when the area is negative.");
-test.todo("should throw an error message when the coveragePerLiter is negative.");
-test.todo("should throw an error message when the coveragePerLiter is zero. Dividing by zero is not possible");
-test.todo("should return expected result when the area is zero.");
+test("should throw an error message when the area is negative.", () => {
+    expect(() => paintRequiredCalculator(-50, 10)).toThrow("Area should be positive");
+});
+
+test("should throw an error message when the coveragePerLiter is negative.", () => {
+  expect(() => paintRequiredCalculator(50, -10)).toThrow("CoveragePerLiter should be greater than zero");
+});
+
+test("should throw an error message when the coveragePerLiter is zero. Dividing by zero is not possible", () => {
+  expect(() => paintRequiredCalculator(50, 0)).toThrow("CoveragePerLiter should be greater than zero");
+});
+
+test("should return expected result when the area is zero.", () => {
+  const result = paintRequiredCalculator(0, 10);
+
+  expect(result).toEqual(0);
+});
 });
