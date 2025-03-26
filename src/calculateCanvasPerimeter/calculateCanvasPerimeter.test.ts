918cffc6c17ced5f2c744e58c6a19ddefb589c02
ValeriaGallegoR <valeriagallego20@gmail.com>
2025-03-26 14:48:47 -0400

    calculateCanvasPerimeter.test.ts: Imports are modified so that they point to the correct file.

--- src/calculateCanvasPerimeter/calculateCanvasPerimeter.test.ts
+++ src/calculateCanvasPerimeter/calculateCanvasPerimeter.test.ts
@@ -1,8 +1,8 @@
-import calculateCanvasSize from "./calculateCanvasSize";
+import calculateCanvasPerimeter from "./calculateCanvasPerimeter";
 
-describe("calculateCanvasSize", () => {
+describe("calculateCanvasPerimeter", () => {
   test("returns correct result", () => {
-    const result = calculateCanvasSize("10", "100");
+    const result = calculateCanvasPerimeter("10", "100");
 
     expect(result).toEqual(1000);
   })
