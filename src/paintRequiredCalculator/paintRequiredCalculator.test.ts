a1a9e58ee206f9c75a07e4e6dbbfd7f4da25b006
ValeriaGallegoR <valeriagallego20@gmail.com>
2025-03-26 15:19:19 -0400

    paintRequiredCalculator.test.ts: Adding todo tests to calculate required paint correctly and with error message for incorrect values.

--- src/paintRequiredCalculator/paintRequiredCalculator.test.ts
+++ src/paintRequiredCalculator/paintRequiredCalculator.test.ts
@@ -6,4 +6,9 @@ describe("paint required calculator", () => {
 
     expect(result).toEqual(5);
   });
+
+test.todo("should throw an error message when the area is negative.");
+test.todo("should throw an error message when the coveragePerLiter is negative.");
+test.todo("should throw an error message when the coveragePerLiter is zero. Dividing by zero is not possible");
+test.todo("should return expected result when the area is zero.");
 });
